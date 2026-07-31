e2799180be3727f28118362cbe163b32cbeec605
Guilherme Fonseca <guilhermefonseca550@gmail.com>
2026-07-31 14:52:32 -0300

    Added integration with mysql

--- src/main/java/br/com/erudio/exception/ResourceNotFoundException.java
+++ src/main/java/br/com/erudio/exception/ResourceNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class UnsupportedMathOperationException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
     public UnsupportedMathOperationException(String message) {
         super(message);
     }
