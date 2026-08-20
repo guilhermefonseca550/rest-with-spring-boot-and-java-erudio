c2c5ce5de4c4264e1d6ab435124bdb2b841333ed
unknown <guilhermefonseca550@gmail.com>
2026-08-20 13:50:05 -0300

    added flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+       (1, 'Leonardo', 'da Vinci', 'Anchiano-Italy', 'Male'),
+        (2, 'Ayrton', 'Senna', 'São Paulo-Brasil', 'Male'),
+        
